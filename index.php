356ce1608069129be32c3459eca76d4fa9e20b37
Dumindu_Ekanayake <137734956+Dumindu-Pabasara@users.noreply.github.com>
2025-04-22 23:34:38 +0530

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,14 @@
     <h1><?php echo"Dumindu Pabasara" ?></h1>
 
 <?php
-    
+    echo("Hello Dumindu!!!");
+    echo 125;
+    echo 45+5;
+
+    //variable declaration
+
+    $my_name = "Dumindu";
+    echo $my_name;
 
     //Assigning value into variable
     $my_name1 = $_GET['userchannel'];
