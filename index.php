c765dd1e29921e7acd8c6d41ff559debdbe9f718
Dumindu_Ekanayake <137734956+Dumindu-Pabasara@users.noreply.github.com>
2025-04-22 21:46:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,13 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?php echo"Dumindu" ?></title>
 </head>
 <body>
     <form >
         <input type="text" name="userchannel">
         <button>Submit</button>
     </form>    
+    <h1><?php echo"Dumindu Pabasara" ?></h1>
 
 <?php
     echo("Hello Dumindu!!!");
@@ -24,6 +25,9 @@
     //Assigning value into variable
     $my_name1 = $_GET['userchannel'];
     echo $my_name1. " is a supper";
+    
+    //This is the single line comment
+    /*This is multiline comment*/
     
 ?>
 
