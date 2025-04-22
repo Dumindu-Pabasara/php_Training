3d3d50824ad906de8aff3c856cd1923b58c42d62
Dumindu_Ekanayake <137734956+Dumindu-Pabasara@users.noreply.github.com>
2025-04-22 05:45:57 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
     echo("Hello Dumindu!!!");
+    echo 125;
+    echo 45+5;
+    
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><? php 
+        Dumindu
+    ?></title>
+</head>
+<body>
+    <h1><?php Dumindu ?></h1>
+</body>
+</html>
