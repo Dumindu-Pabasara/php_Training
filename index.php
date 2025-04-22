9ca61ef023f47f8c266a07b718cc89b034ed9f9b
Dumindu_Ekanayake <137734956+Dumindu-Pabasara@users.noreply.github.com>
2025-04-22 06:38:59 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,31 @@
-<?php
-    echo("Hello Dumindu!!!");
-    echo 125;
-    echo 45+5;
-    
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><? php 
-        Dumindu
-    ?></title>
+    <title>Document</title>
 </head>
 <body>
-    <h1><?php Dumindu ?></h1>
+    <form >
+        <input type="text" name="userchannel">
+        <button>Submit</button>
+    </form>    
+
+<?php
+    echo("Hello Dumindu!!!");
+    echo 125;
+    echo 45+5;
+
+    //variable declaration
+
+    $my_name = "Dumindu";
+    echo $my_name;
+
+    //Assigning value into variable
+    $my_name1 = $_GET['userchannel'];
+    echo $my_name1. " is a supper";
+    
+?>
+
 </body>
 </html>
