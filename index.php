ce950a6b195311da9b4b999b12655a95b02062ca
Dumindu_Ekanayake <137734956+Dumindu-Pabasara@users.noreply.github.com>
2025-04-25 23:38:31 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php $variable = "DTK TV"; echo $variable ?></title>
+</head>
+<body>
 <form >
         <input type="text" name="userchannel">
         <button>Submit</button>
